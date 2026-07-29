c1f426e1158c62ce119e2e51ec798236faea6130
zwol-robotic-proy <zwol.robotic@gmail.com>
2026-07-29 01:09:55 +0000

    Producttion start 2

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -195,7 +195,6 @@ export default function EmailTemplate({
                   ))}
                 </div>
                 </div>
-              ))}
 
               <div className="field">
                 <strong>Mensaje </strong>
